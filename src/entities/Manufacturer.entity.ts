5968ce1ebf9a4e2cb34c69514e0d411ea4b0c874
YoungNaderi <dadynaderi1@gmail.com>
2025-02-25 01:50:37 +0330

    feat: enhance Manufacturer entity with validation and update column types

--- src/entities/Manufacturer.entity.ts
+++ src/entities/Manufacturer.entity.ts
@@ -1,14 +1,20 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Model } from './Model.entity';
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 
 @Entity()
 export class Manufacturer {
   @PrimaryGeneratedColumn()
   id: number;
-  @Column({ type: 'string', nullable: false })
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
   @Column({ type: 'boolean', default: false })
+  @IsBoolean()
   isPartner: boolean;
+
   @OneToMany(() => Model, (models) => models.manufacturer)
   models: Model[];
 }
