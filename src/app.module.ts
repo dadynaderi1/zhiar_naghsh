cd13231edb68b7e70c63dfa5a5d06f1ea6c1ad5f
YoungNaderi <dadynaderi1@gmail.com>
2025-02-24 22:38:57 +0330

    feat: configure global environment variables and TypeORM connection in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ModelModule } from './model/model.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from 'typeorm.config';
 
 @Module({
-  imports: [ModelModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      inject:[ConfigService],
+      useFactory: typeOrmConfig
+    }),
+    ModelModule],
   controllers: [AppController],
   providers: [AppService]
 })
